3a0ea40f454ebe09ae645fad436dc774a720f99e
Egor <el.amegoal@gmail.com>
2025-07-02 14:40:23 +0300

    completed task

--- src/ru/aston/hometask1/LiveLock.java
+++ src/ru/aston/hometask1/LiveLock.java
@@ -1,7 +1,37 @@
 package ru.aston.hometask1;
 
 public class LiveLock {
-    public static void main(String[] args) {
+    static boolean mustWait = true;
 
+    public static void main(String[] args) {
+        new Thread(() -> {
+            while (true) {
+                if (mustWait) {
+                    mustWait = false;
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    mustWait = true;
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            while (true) {
+                if (mustWait) {
+                    mustWait = false;
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    mustWait = true;
+                }
+            }
+        }).start();
     }
 }
