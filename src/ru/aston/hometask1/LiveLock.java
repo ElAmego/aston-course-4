45840181658c394001384c2ad31bc65a0193cead
Egor <el.amegoal@gmail.com>
2025-07-02 14:36:29 +0300

    initial commit

--- src/ru/aston/hometask1/LiveLock.java
+++ src/ru/aston/hometask1/LiveLock.java
@@ -1,37 +1,7 @@
 package ru.aston.hometask1;
 
 public class LiveLock {
-    static boolean mustWait = true;
-
     public static void main(String[] args) {
-        new Thread(() -> {
-            while (true) {
-                if (mustWait) {
-                    mustWait = false;
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                } else {
-                    mustWait = true;
-                }
-            }
-        }).start();
 
-        new Thread(() -> {
-            while (true) {
-                if (mustWait) {
-                    mustWait = false;
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                } else {
-                    mustWait = true;
-                }
-            }
-        }).start();
     }
 }
--- src/ru/aston/hometask2/Main.java
+++ src/ru/aston/hometask2/Main.java
@@ -1,46 +1,7 @@
 package ru.aston.hometask2;
 
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class Main {
-    private static final ReentrantLock lock = new ReentrantLock();
-    private static final Condition condition = lock.newCondition();
-    private static boolean firstTurn = true;
-
     public static void main(String[] args) {
-        new Thread(() ->{
-            while (true) {
-                lock.lock();
-                try {
-                    while (!firstTurn) condition.await();
-                    System.out.println(1);
-                    firstTurn = false;
-                    condition.signal();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    return;
-                } finally {
-                    lock.unlock();
-                }
-            }
-        }).start();
 
-        new Thread(() -> {
-            while (true) {
-                lock.lock();
-                try {
-                    while (firstTurn) condition.await();
-                    System.out.println(2);
-                    firstTurn = true;
-                    condition.signal();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    return;
-                } finally {
-                    lock.unlock();
-                }
-            }
-        }).start();
     }
 }
